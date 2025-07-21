390a021e227ab57cf50ca1d45c615a4eb6dd18b7
Litronom <32793715+Litronom@users.noreply.github.com>
2025-07-21 12:30:28 +0200

    Added POP3 plugins for the latest Tarmac versions

--- README.md
+++ README.md
@@ -1,28 +1,36 @@
 # blender_pop_export
+
 ## Blender addon for .pop files needed for Tarmac (Mario Kart 64 custom course population data)
 
-Necessary for Mario Kart 64 custom course creation, if you're a Blender user.
-The addon will parse a bezier curve for the course's main path and looks for specifically named objects in order to compile everything into a file, which then can be used in Tarmac.
-
-
+Necessary for Mario Kart 64 custom course creation via Blender.
+The addon will parse Bezier Curves for the course's main and alternative paths and it looks for uniquely named objects in order to compile everything into files for Tarmac.
 
 ## Installation:
 
 Install it like any other addon via user preferences and enable the addon.
-File-->Export-->Mario Kart 64 POP (.OK64.POP) will bring up the save dialog.
-Make sure to have your BezierCurve selected as the active object (A reminder message will appear, in case you forget to do that)
+File-->Export-->Mario Kart 64 POP3 (.OK64.POP3) will bring up the save dialog.
 
-For blender versions below 2.8 use **_export_pop_v1_blender.py_**
+For blender versions below 2.8 use ***__blender.py_**
 
-For blender versions 2.8 to 2.92 use **_export_pop_v1_blender2,8.py_**
+For blender versions 2.8 to 2.92 use ***__blender2,8.py_**
 
-For blender versions 2.93 and above use **_export_pop_v1_blender2,93.py_**
-
+For blender versions 2.93 and above use ***__blender2,93.py_**
 
 ![Addon](https://i.imgur.com/wb9fKMQ.png)
 
 ## Usage:
 
-Please refer to this tutorial video to get an idea how to set up your data and export it properly for Tarmac
+**For version 3 of the plugin**:
+In your scene, create an object labeled **"Course Population"**.
+Parent a Bezier Curve to it which will act as your main path for the track. Bezier Curves for alternative paths need to be parented to the main Bezier Curve (up to 3)
+Item boxes, trees, piranha plants, red coins and all other custom objects also need to be parented to **"Course Population"**.
+Name these objects whatever you want, they will be collected in a file as long as the main part of the name is the same for them. For example: **"Mario_Tree1"** and **"Mario_Tree1.001"** will be in the same output file. Blender's standard naming convention for duplication is taken into account.
+Exporting via the save dialog will create a single .PackPOP3 file and multiple separate files, one for each set of uniquely named objects.
+Each of those files can then be imported via Tarmac in order to populate your custom course.
+
+
+
+**For version 1 and 2 of the plugin:**
+Please refer to this tutorial video to get an idea how to set up your data and export it properly for Tarmac:
 
 [![Tutorial](https://i.imgur.com/BQhocIO.png)](https://www.youtube.com/watch?v=2u1L_epK7mg "(Tutorial) MK64 Custom Course Creation - Blender 2.79 [Stream]")
---
+++ export_pop_v3_blender2,93.py
@@ -0,0 +1,203 @@
+bl_info = {
+    "name": "Export .OK64.POP3 Data",
+    "author": "Robichu; Litronom",
+    "version": (3, 0, 0),
+    "blender": (2, 79, 0),
+    "location": "File > Export > Mario Kart 64 POP (.OK64.POP3)",
+    "description": "Export Population Data (.OK64.POP3)",
+    "warning": "",
+    "category": "Import-Export"
+}
+
+import bpy
+import os
+import re
+import time
+from bpy_extras.io_utils import ExportHelper
+from bpy.props import StringProperty, BoolProperty, CollectionProperty
+from bpy.types import Operator, PropertyGroup
+
+
+def export_pop_data(context, self, filepath, course_pop, main_curve, bezier_curves, child_curves):
+    print("running export_pop_data...")
+
+    extension = ".OK64.POP3"
+    directory, filename_with_extension = os.path.split(filepath)
+    filename_without_extension = re.sub(r'\.OK64\.POP3$', '', filename_with_extension)
+    section_name = "path"
+
+    section_filepath = os.path.join(directory, "{}_{}{}".format(filename_without_extension, section_name, extension))
+
+    for name_prop in self.export_names:
+        section_name = name_prop.name
+
+        if name_prop.export:
+            section_filepath = os.path.join(directory, "{}_{}{}".format(filename_without_extension, section_name, extension))
+
+            with open(section_filepath, 'w', encoding='utf-8') as file:
+                if section_name == "path":
+                    loopcount = len(child_curves) + 1
+                    
+                    file.write("{}\n".format(section_name))
+                    file.write("{}\n".format(loopcount))
+                    
+                    pointCount = len(main_curve.data.splines.active.bezier_points)
+                    file.write("{}\n".format(pointCount))
+
+                    for p in main_curve.data.splines.active.bezier_points:
+                        file.write("[{:.6f},{:.6f},{:.6f}]\n1\n".format(p.co.x, p.co.y, p.co.z))
+
+                    for a in child_curves:
+                        pointCount = len(a.data.splines.active.bezier_points)
+                        file.write("{}\n".format(pointCount))
+                        for p in a.data.splines.active.bezier_points:
+                            file.write("[{:.6f},{:.6f},{:.6f}]\n1\n".format(p.co.x, p.co.y, p.co.z))
+
+                else:
+                    names = {}
+                    for obj in course_pop.children:
+                        if obj.type != 'CURVE' and not obj.name.endswith("."):
+                            obj_name = obj.name.split('.')[0]
+                            location = obj.location
+                            if obj_name == section_name:
+                                if section_name not in names:
+                                    names[section_name] = {'count': 0, 'locations': []}
+                                names[section_name]['count'] += 1
+                                names[section_name]['locations'].append(location)
+                    
+                    count = names[section_name]['count']
+                    locations = names[section_name]['locations']
+                    file.write("{}\n{}\n{}\n".format(section_name, 1, count))
+                    for location in locations:
+                        file.write("[{:.6f},{:.6f},{:.6f}]\n1\n".format(location.x, location.y, location.z))
+
+    show_message_box("Pop3 data exported to file", "Finished", 'INFO')
+    return {'FINISHED'}
+
+
+def show_message_box(message="", title="", icon=''):
+    def draw(self, context):
+        self.layout.label(text=message)
+
+    bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
+
+
+class NamePropertyGroup(PropertyGroup):
+    name = StringProperty()
+    description = StringProperty()
+    export = BoolProperty(default=True)
+
+
+class PopExport(Operator, ExportHelper):
+    """Exports .POP3 data for MK64. Requires object named 'Course Population'"""
+    bl_idname = "export_test.some_data"
+    bl_label = "Save OK64.POP3 File"
+    
+    filename_ext = ".OK64.POP3"
+    filter_glob = StringProperty(
+        default="*.OK64.POP3",
+        options={'HIDDEN'},
+        maxlen=255,
+    )
+
+    export_names = CollectionProperty(type=NamePropertyGroup)
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.data.objects.get("Course Population") is not None
+
+    def invoke(self, context, event):
+        course_pop = bpy.data.objects.get("Course Population")
+        if not course_pop:
+            show_message_box("No object named 'Course Population' found", "ERROR", 'ERROR')
+            return {'CANCELLED'}
+        
+        if not course_pop.children:
+            show_message_box("'Course Population' object has no children", "ERROR", 'ERROR')
+            return {'CANCELLED'}
+
+        bezier_curves = [obj for obj in course_pop.children if obj.type == 'CURVE' and obj.data.splines.active.type == 'BEZIER']
+        if not bezier_curves:
+            show_message_box("No Bezier curve found among the children of 'Course Population'", "ERROR", 'ERROR')
+            return {'CANCELLED'}
+        
+        if len(bezier_curves) > 1:
+            show_message_box("More than 1 main Bezier curve found among the children of 'Course Population'", "ERROR", 'ERROR')
+            return {'CANCELLED'}
+
+        main_curve = bezier_curves[0]
+        child_curves = [obj for obj in main_curve.children if obj.type == 'CURVE' and obj.data.splines.active.type == 'BEZIER']
+
+        if len(child_curves) > 3:
+            show_message_box("More than 3 alternative Bezier curves found among the children of the main Bezier curve", "ERROR", 'ERROR')
+            return {'CANCELLED'}
+
+        self.export_names.clear()
+        name_set = set()
+
+        name_set.add("path")
+        name_prop = self.export_names.add()
+        name_prop.name = "path"
+        name_prop.description = "Export path"
+
+        for obj in course_pop.children:
+            if obj.type != 'CURVE' and not obj.name.endswith("."):
+                name = obj.name.split('.')[0]
+                if name not in name_set:
+                    name_set.add(name)
+                    name_prop = self.export_names.add()
+                    name_prop.name = name
+                    name_prop.description = "Export {}".format(name)
+
+        return ExportHelper.invoke(self, context, event)
+
+    def execute(self, context):
+        start_time = time.time()
+        print('\n_____START_____')
+        props = self.export_names
+        filepath = self.filepath
+        filepath = bpy.path.ensure_ext(filepath, self.filename_ext)
+
+        course_pop = bpy.data.objects.get("Course Population")
+        bezier_curves = [obj for obj in course_pop.children if obj.type == 'CURVE' and obj.data.splines.active.type == 'BEZIER']
+        main_curve = bezier_curves[0]
+        child_curves = [obj for obj in main_curve.children if obj.type == 'CURVE' and obj.data.splines.active.type == 'BEZIER']
+
+        exported = export_pop_data(context, self, filepath, course_pop, main_curve, bezier_curves, child_curves)
+        if exported:
+            print('finished export in %s seconds' % (time.time() - start_time))
+            print(filepath)
+        return {'FINISHED'}
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "use_path")
+        for name_prop in self.export_names:
+            row = layout.row()
+            row.prop(name_prop, "export", text=name_prop.name)
+            row.label(text=name_prop.description)
+
+
+def menu_func_export(self, context):
+    if "Course Population" in bpy.data.objects:
+        label = "Mario Kart 64 POP (.OK64.POP3)"
+    else:
+        label = "Mario Kart 64 POP (.OK64.POP3) / missing object: 'Course Population'"
+    self.layout.operator(PopExport.bl_idname, text=label)
+
+
+def register():
+    bpy.utils.register_class(NamePropertyGroup)
+    bpy.utils.register_class(PopExport)
+    bpy.types.INFO_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    bpy.utils.unregister_class(NamePropertyGroup)
+    bpy.utils.unregister_class(PopExport)
+    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+
+
+if __name__ == "__main__":
+    register()
+    bpy.ops.export_test.some_data('INVOKE_DEFAULT')
